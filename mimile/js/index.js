d824572563c24e5edbf366784d3a0c58ce6a461a
liufengjuan1606 <2034280010@qq.com>
2016-12-02 20:59:37 +0800

    第一次提交

---
+++ mimile/js/index.js
@@ -0,0 +1,6 @@
+$(function(){
+	$(".topnav").load("html/public-html/topnav.html");
+	$(".top-bg-form").load("html/public-html/topnav-topbg.html");
+	$("#nav").load("html/public-html/nav.html");
+	$("#erji-list").load("html/public-html/list.html");
+})
